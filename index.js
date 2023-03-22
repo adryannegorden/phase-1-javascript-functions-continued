54da5c3ba0daa3f30b41bfc27467b1409e7a9e63
adryannegorden <adryannegorden@gmail.com>
2023-03-22 02:31:04 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(message = "roller-skate") {
+    return `This Saturday, I want to ${message}!`
+};
+
+function mondayWork(location = "go to the office") {
+    return `This Monday, I will ${location}.`
+};
+
+function wrapAdjective(highlight = "*") {
+    return function(adjective) {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    }
+};
